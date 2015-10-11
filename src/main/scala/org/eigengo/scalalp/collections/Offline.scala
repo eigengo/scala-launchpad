28a488061a79f5a9bf14a6555208e8b04a19de7b
Jan Machacek <jan.machacek@gmail.com>
2015-10-11 19:02:08 +0200

    Improvements

--- src/main/scala/org/eigengo/scalalp/collections/Offline.scala
+++ src/main/scala/org/eigengo/scalalp/collections/Offline.scala
@@ -4,19 +4,18 @@ import scala.io.Source
 
 object Offline extends App {
 
-  lazy val byCategory: Map[String, List[Product]] = products.groupBy(_.shelfCategoryName)
-
   lazy val products: List[Product] = {
     val is = Source.fromInputStream(Offline.getClass.getResourceAsStream("/booze.txt"))
-    is.getLines().flatMap { line =>
-      val arr = line.split('|')
-
-      if (arr.length != 3) None else Some(Product(arr(0), BigDecimal(arr(1)), arr(2)))
+    is.getLines().flatMap { line ⇒
+      line.split('|') match {
+        case Array(name, price, shelfCategoryName) ⇒ Some(Product(name, BigDecimal(price), shelfCategoryName))
+        case _ ⇒ None
+      }
     }.toList
   }
 
   def search(query: String): List[Product] =
-    products.filter(p => p.name.contains(query) || p.shelfCategoryName.contains(query))
+    products.filter(p ⇒ p.name.contains(query) || p.shelfCategoryName.contains(query))
 
   def mostExpensive(query: String): Product =
     search(query).maxBy(_.price)
@@ -28,4 +27,3 @@ object Offline extends App {
   println(mostExpensive("Cider"))
 
 }
-
